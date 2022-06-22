6836e81a9a8fae0298226d65ad9d8cc85b010f39
NETRAMBAKAM BHARATH KUMAR <bharathdarling970070@gmail.com>
2022-06-22 20:42:51 +0530

    this.js

--- module4/preRequisite/this.js
+++ module4/preRequisite/this.js
@@ -1,1 +1,19 @@
 console.log(this); // Outputs an empty object
+
+
+function abc(){
+    console.log("Inside a function 'this' acts as GLobal Object",this);
+}
+abc();
+
+
+let obj = {
+    name : "Amam",
+    age : 23,
+    def : function(){
+        console.log(this);
+    }
+}
+
+obj.def(); //Returns a normal object with name,age anf function
+
