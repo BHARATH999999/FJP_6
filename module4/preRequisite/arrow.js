fe7a5bd5c372ed630a56cb385174044e511c0dce
NETRAMBAKAM BHARATH KUMAR <bharathdarling970070@gmail.com>
2022-06-23 13:14:11 +0530

    arrow.js

--- module4/preRequisite/arrow.js
+++ module4/preRequisite/arrow.js
@@ -12,7 +12,23 @@ add(9,9);
 let arrowfn = () =>{
     console.log(this);
     //returns an empty object;
-    //this in arrow functions depends on its outer scope 
- }
-
+    //this in arrow functions depends on its outer (parent or function)scope i.e lexical scope wise
+    // see below obj example
+}
 arrowfn();
+
+let obj = {
+    name : "Aman",
+    age : 23,
+    showDetails : function(){
+        console.log(this.name + " "+this.age); // returns Aman 23
+    },
+
+    showDetailsArrow:()=>{
+        console.log(this); // returns {}
+        console.log(this.name + " "+this.age); // returns undefined undefined as it has no parent function
+    }
+}
+
+obj.showDetails();
+obj.showDetailsArrow();
