c1681a3ea44131476e9488064e8cdf239301278f
NETRAMBAKAM BHARATH KUMAR <bharathdarling970070@gmail.com>
2022-06-23 12:36:59 +0530

    this.js

--- module4/preRequisite/this.js
+++ module4/preRequisite/this.js
@@ -1,21 +1,34 @@
+// this keyword is for reference purpose to any object
+// this keyword's value will depend upon how it is called ?
+// Two types -->
+// 1.Function Invocation
+// 2.Method Invocation
+
+
 console.log(this); // Outputs an empty object
 
 
 function abc(){
     console.log("Inside a function 'this' acts as GLobal Object",this);
 }
-abc();
+abc(); //function invocation
 
 
 let obj = {
     name : "Amam",
     age : 23,
     def : function(){
-        console.log(this);
+        // console.log(this);
+        function klm(){
+            console.log(this);
+        }
+        klm(); // function invocation
     }
 }
 
-obj.def(); //Returns a normal object with name,age and function
-
+obj.def(); //method invocation
+//Returns a normal object with name,age and function
+  
 let ghi = obj.def; //making a copy of def function to ghi, now ghi is a function
-ghi(); //Now this acts as global object
+ghi(); //function invocation
+//Now this acts as global object
