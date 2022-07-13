745b7cd48dbc95ba08c1ad02f8d8dff85c66514e
NETRAMBAKAM BHARATH KUMAR <bharathdarling970070@gmail.com>
2022-07-13 22:55:56 +0530

    Favourites UI completed

--- module4/react/movies-app/src/App.js
+++ module4/react/movies-app/src/App.js
@@ -2,13 +2,15 @@ import NavBar from './components/NavBar.js';
 import './App.css';
 import Banner from './components/Banner.js';
 import MovieList from './components/MovieList.js';
+import Fav from './components/Fav.js';
 
 function App() {
   return (
     <>
       <NavBar/>
-      <Banner/>
-      <MovieList/>
+      {/* <Banner/> */}
+      {/* <MovieList/> */}
+      <Fav/>
     </>
   );
 }
