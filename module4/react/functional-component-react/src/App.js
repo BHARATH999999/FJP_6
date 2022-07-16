595e9323894dbd7ca0494e3a21cd7329db37ddda
NETRAMBAKAM BHARATH KUMAR <bharathdarling970070@gmail.com>
2022-07-16 09:14:30 +0530

    useEffect

--- module4/react/functional-component-react/src/App.js
+++ module4/react/functional-component-react/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/counter';
+import Ue1 from './Components/ue1';
 
 function App() {
   return (
-    <Counter/>
+    // <Counter/>
+    <Ue1/>
   );
 }
 
