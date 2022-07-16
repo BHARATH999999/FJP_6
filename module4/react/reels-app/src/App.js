3b94cf5870ac4bf83fa8c47d2469e1199eac9fb5
NETRAMBAKAM BHARATH KUMAR <bharathdarling970070@gmail.com>
2022-07-16 21:42:28 +0530

    reels-app started and routing done

---
+++ module4/react/reels-app/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import Feed from './components/Feed'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import PageNotFound from "./components/PageNotFound"
+import SignUp from './components/Signup'
+import { Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/feed">
+        <Feed></Feed>
+      </Route>
+      <Route path="/login">
+        <Login></Login>
+      </Route>
+      <Route path="/signup">
+        <SignUp>
+
+        </SignUp>
+      </Route>
+
+      <Route path="/profile">
+        <Profile>
+
+        </Profile>
+      </Route>
+      <Route>
+        <PageNotFound>
+
+        </PageNotFound>
+      </Route>
+    </Switch>
+  );
+}
+
+export default App;
