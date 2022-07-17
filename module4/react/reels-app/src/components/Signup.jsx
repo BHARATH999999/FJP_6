b983c27796f435df56e9d2f95347afac675f650b
NETRAMBAKAM BHARATH KUMAR <bharathdarling970070@gmail.com>
2022-07-17 17:22:48 +0530

    Sign Up

--- module4/react/reels-app/src/components/Signup.jsx
+++ module4/react/reels-app/src/components/Signup.jsx
@@ -1,8 +1,23 @@
-
+import { useState } from "react"
+import {createUserWithEmailAndPassword} from "firebase/auth"
+import {auth} from "../firebase"
 
 function Signup(){
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [name,setName] = useState("")
+
+    function processSignUp(){
+        let userCred = createUserWithEmailAndPassword(auth,email,password);
+        console.log(userCred);
+    }
     return(
-        <div>Sign up</div>
+        <>
+            <input type="email" value = {email} onChange ={(e) =>setEmail(e.target.value)} placeholder = "email"></input><br></br>
+            <input type="password" value = {password} onChange ={(e) =>setPassword(e.target.value)} placeholder = "password"></input><br></br>
+            <input type="text" value = {name} onChange ={(e) =>setName(e.target.value)}placeholder = "Full Name"></input><br></br>
+            <button type = "click" onClick={processSignUp}>Sign Up</button>
+        </>
     )
 }
 
