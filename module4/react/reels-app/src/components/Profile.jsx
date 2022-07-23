cc89aa7723ce44039210c4ed84e4af6b1dac3067
Bharath <bharathdarling970070@gmail.com>
2022-07-23 23:26:25 +0530

    profile page sample html css

--- module4/react/reels-app/src/components/Profile.jsx
+++ module4/react/reels-app/src/components/Profile.jsx
@@ -1,8 +1,20 @@
+import "./profile.css"
 
-
-function Profile(){
-    return(
-        <div>Profile</div>
+function Profile() {
+    return (
+        <>
+            <div className="header"></div>
+            <div className="main">
+                <div className="profileImgContainer">
+                    <img src="https://assets.leetcode.com/users/avatars/avatar_1648218219.png" alt="" className="pimg" />
+                </div>
+                <div className="details">
+                    <div className="content">Bharath</div>
+                    <div className="content">No of posts: <span className="bold_text">Posts</span></div>
+                    <div className="content">Email <span className="bold_text">Email.com</span></div>
+                </div>
+            </div>
+        </>
     )
 }
 
