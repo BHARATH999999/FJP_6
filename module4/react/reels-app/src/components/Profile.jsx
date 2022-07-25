1a68203ea7afdb5d143f449fbd97d732cb8b2126
Bharath <bharathdarling970070@gmail.com>
2022-07-26 00:42:34 +0530

    profile

--- module4/react/reels-app/src/components/Profile.jsx
+++ module4/react/reels-app/src/components/Profile.jsx
@@ -1,29 +1,31 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./profile.css"
-import { useContext } from "react"
+import {useContext} from "react"
 import {AuthContext} from "../context/Authcontext"
+import {db} from "../firebase"
+import {doc,getDoc} from "firebase/firestore"
 
-function Profile() {
-
+function Profile(){
     let cUser = useContext(AuthContext);
+    let [loading,setLoading] = useState("")
+
     return (
-        <>
-            {cUser == null ? <div>Need to Login</div> :
-                <div> Logged in user is {cUser.uid}</div>}
+            <>
+                {cUser == null?<div>Need to login</div>:    
+         <>
+         <div className="header"></div>
+         <div className="main">
+             <div className="pimg_container">
+                 <img src="https://assets.leetcode.com/users/avatars/avatar_1648218219.png" alt="" className="pimg" />
+             </div>
+             <div className="details">
+                 <div className="content">Aman</div>
+                 <div className="content">No of Posts: <span className="bold_text">Posts</span></div>
+                 <div className="content">Email <span className="bold_text">Email.com</span></div>
+             </div>
+         </div>
+         </>}
         </>
-        // <>
-        //     <div className="header"></div>
-        //     <div className="main">
-        //         <div className="profileImgContainer">
-        //             <img src="https://assets.leetcode.com/users/avatars/avatar_1648218219.png" alt="" className="pimg" />
-        //         </div>
-        //         <div className="details">
-        //             <div className="content">Bharath</div>
-        //             <div className="content">No of posts: <span className="bold_text">Posts</span></div>
-        //             <div className="content">Email <span className="bold_text">Email.com</span></div>
-        //         </div>
-        //     </div>
-        // </>
     )
 }
 
