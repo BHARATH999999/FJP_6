92f02f00d5667daf100c3e229ba1052ee1c14d57
Bharath <bharathdarling970070@gmail.com>
2022-07-28 22:11:21 +0530

    profile update

--- module4/react/reels-app/src/components/Profile.jsx
+++ module4/react/reels-app/src/components/Profile.jsx
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from "react"
 import "./profile.css"
-import {useContext} from "react"
-import {AuthContext} from "../context/Authcontext"
-import {db} from "../firebase"
-import {doc,getDoc} from "firebase/firestore"
+import { useContext } from "react"
+import { AuthContext } from "../context/Authcontext"
+import { db } from "../firebase"
+import { doc, getDoc } from "firebase/firestore"
 import { async } from "@firebase/util"
 
-function Profile(){
+function Profile() {
     let cUser = useContext(AuthContext);
-    let [loading,setLoading] = useState("")
+    let [loading, setLoading] = useState(true)
+    let [user, setUser] = useState(null);
 
-    useEffect(function(){
-        (async function(){
-            if(cUser){
+    useEffect(function () {
+        (async function () {
+            if (cUser) {
                 //Read from database
-                const docRef = doc(db,"users",cUser.uid);
-                const docSnap = await getDoc(docRef);
-                console.log("Document Data : ",docSnap);
+                const docRef = doc(db, "users", cUser.uid);
+                const userObj = await getDoc(docRef);
+                console.log("Document Data : ", userObj.data);
+                setUser(userObj.data);
+                setLoading(false);
             }
         })()
-    },[cUser])
+    }, [])
     return (
-            <>
-                {cUser == null?<div>Need to login</div>:    
-         <>
-         <div className="header"></div>
-         <div className="main">
-             <div className="pimg_container">
-                 <img src="https://assets.leetcode.com/users/avatars/avatar_1648218219.png" alt="" className="pimg" />
-             </div>
-             <div className="details">
-                 <div className="content">Aman</div>
-                 <div className="content">No of Posts: <span className="bold_text">Posts</span></div>
-                 <div className="content">Email <span className="bold_text">Email.com</span></div>
-             </div>
-         </div>
-         </>}
+        <>
+            {loading == true ? <div>...Loading</div> :
+                <>
+                    <div className="header"></div>
+                    <div className="main">
+                        <div className="pimg_container">
+                            <img src={user.profileImgUrl} alt="" className="pimg" />
+                        </div>
+                        <div className="details">
+                            <div className="content">{user.name}</div>
+                            <div className="content">No of Posts: {user.reelsIds.length}<span className="bold_text">Posts</span></div>
+                            <div className="content">{user.email} <span className="bold_text">Email.com</span></div>
+                        </div>
+                    </div>
+                </>}
         </>
     )
 }
